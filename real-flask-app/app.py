971c83c9ba7a13a229c6592b544cee380a0e121c
tiju46 <pttiju@gmail.com>
2026-06-30 18:09:18 +0100

    add render_template and css

--- real-flask-app/app.py
+++ real-flask-app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Tiju! Your REAL Flask app is deployed successfully 🚀"
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
